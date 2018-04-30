c11f2e55471d906b33084133cfcd30a3495e9044
Joe Grandja <jgrandja@pivotal.io>
2018-04-30 19:57:39 -0400

    Sample for mapping user authorities

---
+++ src/main/java/sample/data/UserAuthority.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+/**
+ * Represents a simple authority for a specific {@link #getUserId() User}.
+ *
+ * @author Joe Grandja
+ */
+@Entity
+public class UserAuthority implements Serializable {
+	private static final long serialVersionUID = 2738828949330833739L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotNull
+	private String userId;
+
+	@NotEmpty(message = "Authority is required.")
+	private String authority;
+
+	public UserAuthority() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user_authority(id,user_id,authority) values (0,'springsec5@pivotal.io','CREATE_MESSAGE');
+insert into user_authority(id,user_id,authority) values (1,'springsec5@pivotal.io','READ_MESSAGE');
+insert into user_authority(id,user_id,authority) values (2,'springsec5@pivotal.io','UPDATE_MESSAGE');
+insert into user_authority(id,user_id,authority) values (3,'springsec5@pivotal.io','DELETE_MESSAGE');
